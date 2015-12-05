5a9b732464ae89eaab36131f87b52fd5ca895e8e
Brett Henderson <bretth18@gmail.com>
2015-12-05 10:01:38 -0800

    color changes
    changing them colors

--- public/main.js
+++ public/main.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to Troper a0.0.1 – ";
     log(message, {
       prepend: true
     });
